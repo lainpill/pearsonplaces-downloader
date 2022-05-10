4d30a55a60837f031d8eb2d14ac1322173557abd
ch7rms <102785487+ch7rms@users.noreply.github.com>
2022-05-10 16:17:04 +1000

    Update index.js
    fuck https (axios >)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var fs = require('fs')
-const http = require('https');
+const axios = require('axios')
 readlineSync = require('readline-sync');
 fs = require('fs');
 var completed = -1;
@@ -12,6 +12,7 @@ var completed = -1;
   var bookid = readlineSync.question('enter book id: ');
   var bookpg = readlineSync.question('enter book pagecount: ');
   var pdf = new (require('pdfkit'))({
+    compress:false,
     autoFirstPage: false,
       size: "A4",
       layout: "portrait",
@@ -23,24 +24,24 @@ var completed = -1;
     var bookorg = bookpg;
 
     setInterval(function() {
-        if(fs.existsSync('./page'+bookorg+'.PNG') && completed == bookorg){
+        if(fs.existsSync('./page'+bookorg+'.jpg') && completed == bookorg){
             for (let i = 0; i < bookpg; i++) {
-                var img = pdf.openImage('./page' +i+'.PNG');
+                var img = pdf.openImage('./page' +i+'.jpg');
                 pdf.addPage({size: [img.width, img.height]});
                 pdf.image(img, 0, 0);
             }
         if(pdf.bufferedPageRange().count === bookpg) {
             console.log("\x1b[31m", '');
-            console.log("finished converting! deleting temp pngs.");
+            console.log("finished converting! deleting temp jpgs.");
             for (let i = 0; i < bookpg; i++) {
-                fs.unlink('page'+i+'.PNG', function (err) {
+                fs.unlink('page'+i+'.jpg', function (err) {
                     if (err) throw err;
-                    console.log('deleted: page' + i + ".PNG");
+                    console.log('deleted: page' + i + ".jpg");
                 });
             }
             console.log("\x1b[0m", '');
             setTimeout(function() {
-                pdf.end();
+                if(pdf.readable) {pdf.end();}
                 console.log("finished pdf, you can close application now :)")
             },5000);
         }}
@@ -50,14 +51,19 @@ var completed = -1;
       for (let i = 0; i < bookpg; i++) {
         setTimeout(
             function(){
-                const file = fs.createWriteStream('page' + i + '.PNG');
-                    const request = http.get('https://d2f01w1orx96i0.cloudfront.net/resources/products/epubs/generated/' + bookid + '/foxit-assets/pages/page' + i, function(response) {
-                        response.pipe(file);
-                        file.on("finish", () => {
+                axios({
+                    method: "get",
+                    responseType: 'stream',
+                    url: 'https://d2f01w1orx96i0.cloudfront.net/resources/products/epubs/generated/' + bookid + '/foxit-assets/pages/page' + i,
+                }).then(function (response) {
+                    response.data.pipe(fs.createWriteStream('page' + i + '.jpg'))
+                        .on('error', () => {
+                            console.log('error downloading page: ' + i);
+                        })
+                        .on("finish", () => {
                             completed++;
-                            file.close();
                             console.log("download " + i + " completed");
                         });
-                    });
+                });
             },50 * i)
         }
